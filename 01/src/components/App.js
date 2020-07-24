a463d53adc18e15a761a84445a6615a375a6a581
erreinoso <eramirezreinoso@gmail.com>
2020-07-24 12:37:04 +0200

    object to cardmaker

--- 01/src/components/App.js
+++ 01/src/components/App.js
@@ -3,19 +3,6 @@ import '../stylesheets/App.scss';
 import CardMaker from './CardMaker/CardMaker';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      palette: '', //antes colorSelected
-      name: '',
-      job: '',
-      photo: '',
-      email: '',
-      phone: '', //antes telephone
-      linkedin: '', //antes linkedIn
-      github: '', //antes gitHubZ
-    };
-  }
   render() {
     return (
       <div className="App">
--- 01/src/components/CardMaker/CardMaker.js
+++ 01/src/components/CardMaker/CardMaker.js
@@ -1,12 +1,25 @@
-import React from "react";
-import Form from "./Form";
-import Preview from "./Preview";
-import Footer from "../Footer";
-import Header from "../Header";
+import React from 'react';
+import Form from './Form';
+import Preview from './Preview';
+import Footer from '../Footer';
+import Header from '../Header';
 
 class CardMaker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      palette: '', //antes colorSelected
+      name: '',
+      job: '',
+      photo: '',
+      email: '',
+      phone: '', //antes telephone
+      linkedin: '', //antes linkedIn
+      github: '', //antes gitHubZ
+    };
+  }
   render() {
-    console.log("funciona");
+    console.log('funciona');
     return (
       <div>
         <Header />
