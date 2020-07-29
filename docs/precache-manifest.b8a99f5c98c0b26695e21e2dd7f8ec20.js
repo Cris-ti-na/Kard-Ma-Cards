97a3d7a54d288d09794c93bb714bc0db1eac08a6
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-07-29 12:18:10 +0200

    Update docs

---
+++ docs/precache-manifest.b8a99f5c98c0b26695e21e2dd7f8ec20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7afd1158a60962832452e62f3f37a08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30bf2412bb3984152ca2",
+    "url": "./static/css/main.94d86588.chunk.css"
+  },
+  {
+    "revision": "dc91834aa33af8923e41",
+    "url": "./static/js/2.f93c5729.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f93c5729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30bf2412bb3984152ca2",
+    "url": "./static/js/main.48cc90ca.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  },
+  {
+    "revision": "c02e56bff883a33da375f6ab5ff5efb9",
+    "url": "./static/media/default-image-flower-fill.c02e56bf.svg"
+  },
+  {
+    "revision": "6c58690a64b4e251012d7f6a95cece8a",
+    "url": "./static/media/logo-kardMA-hand.6c58690a.svg"
+  },
+  {
+    "revision": "4f72f8ccc3d75bc58373547fae9a8304",
+    "url": "./static/media/logo-kardMA.4f72f8cc.png"
+  }
+]);
